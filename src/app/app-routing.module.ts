bf3118a003ee58e7987619b238629ed642ba6de8
Nicolas Ysidoro Oris <nicolasoris98@gmail.com>
2023-10-21 14:11:27 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "products", loadComponent:
+      () => import("./modules/product-list/product-list.component")
+        .then(c => c.ProductListComponent)
+  },
+  { path: "", pathMatch: "full", redirectTo: "products" },
+  { path: "**", pathMatch: "full", redirectTo: "products" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/product-edit/product-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-edit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss']
+})
+export class ProductEditComponent {
+
+}
